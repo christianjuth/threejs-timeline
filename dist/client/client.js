9e8d4a664855795533ff47fa84f293259580ac28
Christian Juth <cjuth2@gmail.com>
2021-05-19 11:30:31 -0400

    chore: add uncommited files

--- dist/client/client.js
+++ dist/client/client.js
@@ -1,16 +1,46 @@
 import * as THREE from '/build/three.module.js';
-import { OrbitControls } from '/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 50, 100);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls(camera, renderer.domElement);
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 2;
+// const controls = new OrbitControls(camera, renderer.domElement)
+// camera.position.x = Math.PI / 2
+// camera.rotation.z -= 0.0001
+// MATERIALS
+// const material = new THREE.LineBasicMaterial({
+// 	color: 0xff00ff
+// });
+// DATA
+// type PointData = [number, number, number]
+// type LineData = [PointData, PointData]
+// const data: LineData[] = [
+//   [[0, 0, 0], [0, -900, 0]]
+// ]
+// for (const lineData of data) {
+//   const points = []
+//   for (const pointData of lineData) { 
+//     points.push( new THREE.Vector3(...pointData) )
+//     points.push( new THREE.Vector3(...pointData) )
+//   }
+//   const geometry = new THREE.BufferGeometry().setFromPoints( points );
+//   const line = new THREE.Line( geometry, material );
+//   scene.add( line );
+// }
+for (let i = 0; i < 200; i++) {
+    const color = new THREE.Color(0xffffff);
+    // color.setHex( Math.random() * 0xffffff );
+    const material = new THREE.LineBasicMaterial({
+        color
+    });
+    const points = [];
+    points.push(new THREE.Vector3(-100, 0, -i * 10));
+    points.push(new THREE.Vector3(100, 0, -i * 10));
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const line = new THREE.Line(geometry, material);
+    scene.add(line);
+}
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -20,9 +50,8 @@ function onWindowResize() {
 }
 var animate = function () {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    controls.update();
+    // controls.update()
+    // camera.translateZ( -1 )
     render();
 };
 function render() {
--- src/client/client.ts
+++ src/client/client.ts
@@ -4,41 +4,88 @@ import { OrbitControls } from '/jsm/controls/OrbitControls'
 const scene: THREE.Scene = new THREE.Scene()
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set( 0, 50, 100 );
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const controls = new OrbitControls(camera, renderer.domElement)
+// const controls = new OrbitControls(camera, renderer.domElement)
+// camera.position.x = Math.PI / 2
+// camera.rotation.z -= 0.0001
 
-const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 
-const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
-scene.add(cube)
+// MATERIALS
 
-camera.position.z = 2
+// const material = new THREE.LineBasicMaterial({
+// 	color: 0xff00ff
+// });
+
+// DATA
+
+// type PointData = [number, number, number]
+// type LineData = [PointData, PointData]
+
+// const data: LineData[] = [
+//   [[0, 0, 0], [0, -900, 0]]
+// ]
+
+
+
+// for (const lineData of data) {
+//   const points = []
+
+//   for (const pointData of lineData) { 
+//     points.push( new THREE.Vector3(...pointData) )
+//     points.push( new THREE.Vector3(...pointData) )
+//   }
+
+//   const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+//   const line = new THREE.Line( geometry, material );
+//   scene.add( line );
+// }
+
+
+
+for (let i = 0; i < 200; i++) {
+  const color = new THREE.Color( 0xffffff );
+  // color.setHex( Math.random() * 0xffffff );
+
+  const material = new THREE.LineBasicMaterial({
+    color
+  });
+
+  const points = []
+
+  points.push( new THREE.Vector3(-100, 0, -i * 10) )
+  points.push( new THREE.Vector3(100, 0, -i * 10) )
+
+  const geometry = new THREE.BufferGeometry().setFromPoints( points )
+
+  const line = new THREE.Line( geometry, material )
+  scene.add( line )
+}
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  render()
 }
 
 var animate = function () {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+  // controls.update()
 
-    controls.update()
+  // camera.translateZ( -1 )
 
-    render()
+  render()
 };
 
 function render() {
-    renderer.render(scene, camera)
+  renderer.render(scene, camera)
 }
 animate();
